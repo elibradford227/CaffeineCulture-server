cee85671e6376c38564fb314099b0a5563f54c21
Eli Bradford <elibradford227@gmail.com>
2024-02-13 19:48:47 -0600

    fixtures added and loaded

--- caffeineculture/urls.py
+++ caffeineculture/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
 from caffeinecultureapi.views import check_user, register_user
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- caffeinecultureapi/models/post.py
+++ caffeinecultureapi/models/post.py
@@ -6,6 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=20)
     content = models.CharField(max_length=1000)
     date = models.DateTimeField(auto_now_add=True)
-    like_count = 0
+    like_count = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
