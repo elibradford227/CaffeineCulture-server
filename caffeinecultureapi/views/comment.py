1a5136145d21c81fe4a388d944725fbebd04a146
Eli Bradford <elibradford227@gmail.com>
2024-02-22 13:39:10 -0600

    implemented comment replies on comments

--- caffeinecultureapi/views/comment.py
+++ caffeinecultureapi/views/comment.py
@@ -2,11 +2,19 @@ from django.http import HttpResponseServerError
 from django.db.models import Count
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import serializers, status
 from caffeinecultureapi.models import Post, User, Category, Comment
 
 class CommentView(ViewSet):
     """Level up comment view"""
+    
+    # def __serialize_nested__(self, comment):
+    #     serialized_list = []
+    #     serialized = CommentSerializer(comment, many=True).data
+    #     serialized_list.append(serialized)
+    #     for child in 
+    #     return serialized_list
 
     def retrieve(self, request, pk):
         """Handle GET requests for single comments
@@ -16,8 +24,15 @@ class CommentView(ViewSet):
         """
         try:
             comment = Comment.objects.get(pk=pk)
-            serializer = CommentSerializer(comment, context={'request': request})
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            serialized = []
+            if comment.parent is None:
+                comment_family = comment.get_children()
+                print(comment_family)
+                serialized_family = CommentSerializer(comment_family, many=True).data
+                serialized.extend(serialized_family)
+            # serializer = CommentSerializer(comment, context={'request': request})
+            print(serialized)
+            return Response(serialized, status=status.HTTP_200_OK)
         except Comment.DoesNotExist as ex:
             return Response({'comment does not exist': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
@@ -28,9 +43,16 @@ class CommentView(ViewSet):
         Returns:
             Response -- JSON serialized list of comments
         """
-        comment = Comment.objects.all()
-        serializer = CommentSerializer(comment, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        comments = Comment.objects.all()
+        serialized = []
+        
+        for comment in comments:
+            if comment.parent is None:
+                comment_family = comment.get_children()
+                serialized_family = CommentSerializer(comment_family, many=True).data
+                serialized.extend(serialized_family)
+        
+        return Response(serialized, status=status.HTTP_200_OK)
       
     def create(self, request):
         """Handle comment operations
@@ -82,6 +104,54 @@ class CommentView(ViewSet):
         comment = Comment.objects.get(pk=pk)
         comment.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    @action(methods=['post'], detail=True)
+    def create_reply(self, request, pk):
+        """Creates a reply comment
+        Returns:
+            Response: Success message with 204 code
+        """
+        try:
+            parent_comment = Comment.objects.get(pk=pk)
+            
+            user=User.objects.get(uid=request.data["uid"])
+            post=Post.objects.get(pk=request.data["post"])
+        
+            comment = Comment.objects.create(
+                user = user,
+                post = post,
+                content = request.data["content"],
+                parent = parent_comment
+            )
+
+            serializer = CommentSerializer(comment, many=False)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except User.DoesNotExist:
+            return Response('User not found', status=status.HTTP_404_NOT_FOUND)
+        except Post.DoesNotExist:
+            return Response('Post not found', status=status.HTTP_404_NOT_FOUND)
+    
+    @action(methods=['get'], detail=True)
+    def get_single_post_comments(self, request, pk):
+        """Returns a single posts comments with nested replies
+        Returns:
+            Response: Success message with 204 code
+        """
+        
+        post = Post.objects.get(pk=pk)
+        comments = Comment.objects.filter(post=post).order_by('-id')
+        
+        serialized = []
+        
+        for comment in comments:
+            if comment.parent is None:
+                comment_family = comment.get_children()
+                serialized_family = CommentSerializer(comment_family, many=True).data
+                serialized.extend(serialized_family)
+        
+        return Response(serialized, status=status.HTTP_200_OK)
+        
+
         
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -90,7 +160,8 @@ class CommentSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Comment
-        fields = ('id', 'user', 'post', 'content', 'date')
-        depth = 1
+        fields = ('id', 'user', 'post', 'content', 'date', 'parent')
+        depth = 2
+        
 
         
