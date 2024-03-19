d9cc8d436891c34bb1e0a9adbc857d6a385846a2
Eli Bradford <elibradford227@gmail.com>
2024-03-18 20:27:34 -0500

    added search call

--- caffeinecultureapi/views/helpers.py
+++ caffeinecultureapi/views/helpers.py
@@ -3,8 +3,8 @@ from rest_framework import status
 from caffeinecultureapi.models import User
 
 def get_user_by_uid(uid):
-      try:
-          user = User.objects.get(uid=uid)
-          return user
-      except User.DoesNotExist:
-          return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    try:
+        user = User.objects.get(uid=uid)
+        return user
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
